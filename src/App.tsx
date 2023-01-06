bfcd30b11613600ef7852e1867def7ec4dd6322c
Bruno Malagoli <bmalagoli2@gmail.com>
2023-01-06 19:50:07 -0300

    Hero component first structuring

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
       <NavBar />
+      <Hero />
     </>
   );
 }
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,10 @@
+import styles from "./styles/index.module.css";
+const Hero = () => {
+  return (
+    <section className={styles.heroContainer}>
+      <div className={styles.heroCard}></div>
+    </section>
+  );
+};
+
+export default Hero;
