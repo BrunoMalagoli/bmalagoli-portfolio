eb6082d150dbb20773a5824b75ba5244d71612b0
Bruno Malagoli <bmalagoli2@gmail.com>
2023-01-11 19:48:34 -0300

    Styling changes (color pallete and layout)

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -2,8 +2,14 @@ import styles from "./styles/index.module.css";
 const Hero = () => {
   return (
     <div className={styles.heroContainer}>
-      <section>
+      <section id={styles.mainContent}>
         <h1 id={styles.mainText}>Bruno Malagoli</h1>
+        <h3 className={styles.subText}>Full-Stack Web Developer</h3>
+      </section>
+      <section>
+        <div id={styles.profileImg}>
+          <img height={"300px"} width={"300px"} src="/profile.jpg"></img>
+        </div>
       </section>
     </div>
   );
