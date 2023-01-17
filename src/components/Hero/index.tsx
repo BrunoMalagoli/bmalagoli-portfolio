aa6d1c441b9162f9e6a74fd54357dbbd851f6645
Bruno Malagoli <bmalagoli2@gmail.com>
2023-01-17 19:19:04 -0300

    Responsive Design for Hero section

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,14 +1,16 @@
 import styles from "./styles/index.module.css";
 const Hero = () => {
   return (
-    <div className={styles.heroContainer}>
+    <div id={styles.heroContainer}>
       <section id={styles.mainContent}>
-        <h1 id={styles.mainText}>Bruno Malagoli</h1>
-        <h3 className={styles.subText}>Full-Stack Web Developer</h3>
-      </section>
-      <section>
+        <div id={styles.textContainer}>
+          <h1 id={styles.mainText}>Bruno Malagoli</h1>
+          <h3 className={styles.subText}>Full-Stack Web Developer</h3>
+        </div>
         <div id={styles.profileImg}>
-          <img height={"300px"} width={"300px"} src="/profile.jpg"></img>
+          <div>
+            <img height={"300px"} width={"300px"} src="/profile2.jpg"></img>
+          </div>
         </div>
       </section>
     </div>
