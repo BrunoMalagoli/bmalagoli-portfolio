1b234436232655ab513484a58931421894bf7048
Bruno Malagoli <bmalagoli2@gmail.com>
2023-01-18 20:11:30 -0300

    Media buttons implementation

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,11 +1,20 @@
 import styles from "./styles/index.module.css";
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 const Hero = () => {
   return (
     <div id={styles.heroContainer}>
       <section id={styles.mainContent}>
         <div id={styles.textContainer}>
-          <h1 id={styles.mainText}>Bruno Malagoli</h1>
-          <h3 className={styles.subText}>Full-Stack Web Developer</h3>
+          <h1 id={styles.mainText}>
+            Bruno <span className={styles.highlight}>Malagoli</span>
+          </h1>
+          <h3 className={styles.subText}>
+            Full-Stack <span className={styles.highlight}>Web Developer</span>
+          </h3>
+          <div id={styles.mediaContainer}>
+            <AiFillLinkedin fontSize={"25px"} color="#b3ffb2" />
+            <AiFillGithub fontSize={"25px"} color="#b3ffb2" />
+          </div>
         </div>
         <div id={styles.profileImg}>
           <div>
