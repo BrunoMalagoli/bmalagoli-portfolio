e25de657665b2ccc46461d536782ba09e9681818
Bruno Malagoli <bmalagoli2@gmail.com>
2023-07-30 00:06:22 -0300

    Fade animations

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -3,26 +3,29 @@ import MainText from "./components/MainText";
 import ParticleElement from "./components/ParticleElement";
 import HeroStyles from "./styles/index.module.css";
 import { Link, Element } from "react-scroll";
+import { Fade } from "react-awesome-reveal";
 const Hero = () => {
   return (
     <Element name="inicio">
       <div className={HeroStyles.particles_container}>
         <ParticleElement />
       </div>
-      <section className={HeroStyles.hero_container}>
-        <MainText />
-        <div className={HeroStyles.down_button}>
-          <Link
-            activeClass="active"
-            spy={true}
-            smooth={true}
-            duration={200}
-            to="about"
-          >
-            <IconArrowDownCircle />
-          </Link>
-        </div>
-      </section>
+      <Fade delay={800}>
+        <section className={HeroStyles.hero_container}>
+          <MainText />
+          <div className={HeroStyles.down_button}>
+            <Link
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              duration={200}
+              to="about"
+            >
+              <IconArrowDownCircle />
+            </Link>
+          </div>
+        </section>
+      </Fade>
     </Element>
   );
 };
--- src/components/ProjectsSection/index.tsx
+++ src/components/ProjectsSection/index.tsx
@@ -7,46 +7,49 @@ import {
   IconReact,
   IconTypescript,
 } from "../EducationSection/components/TechStack/IconComponents";
+import { Fade } from "react-awesome-reveal";
 
 const ProjectsSection = () => {
   return (
     <Element name="proyectos">
-      <section>
-        <div className={ProjectsStyles.projects_container}>
-          <h2>Proyectos</h2>
-          <ProjectCard
-            title="nin-js"
-            description="Web app estilo Quiz-game con preguntas de programación"
-            tech={[
-              <IconReact height={"3rem"} width={"3rem"} />,
-              <IconTypescript height={"3rem"} width={"3rem"} />,
-              <IconExpress height={"3rem"} width={"3rem"} />,
-              <IconMongodb height={"3rem"} width={"3rem"} />,
-            ]}
-            features={[
-              "Registro y Login",
-              "Niveles de dificultad",
-              "Sistema de progreso",
-              "Personalización de perfil con avatar y nombre de usuario",
-            ]}
-            url="https://nin-js.netlify.app/"
-            repoUrl="https://github.com/BrunoMalagoli/NinJS-Vite"
-          />
-          <ProjectCard
-            title="nin-js"
-            description="Web app estilo Quiz-game con preguntas de programación"
-            tech={[
-              <IconReact height={"3rem"} width={"3rem"} />,
-              <IconTypescript height={"3rem"} width={"3rem"} />,
-              <IconExpress height={"3rem"} width={"3rem"} />,
-              <IconMongodb height={"3rem"} width={"3rem"} />,
-            ]}
-            features={[""]}
-            url=""
-            repoUrl=""
-          />
-        </div>
-      </section>
+      <Fade>
+        <section>
+          <div className={ProjectsStyles.projects_container}>
+            <h2>Proyectos</h2>
+            <ProjectCard
+              title="nin-js"
+              description="Web app estilo Quiz-game con preguntas de programación"
+              tech={[
+                <IconReact height={"3rem"} width={"3rem"} />,
+                <IconTypescript height={"3rem"} width={"3rem"} />,
+                <IconExpress height={"3rem"} width={"3rem"} />,
+                <IconMongodb height={"3rem"} width={"3rem"} />,
+              ]}
+              features={[
+                "Registro y Login",
+                "Niveles de dificultad",
+                "Sistema de progreso",
+                "Personalización de perfil con avatar y nombre de usuario",
+              ]}
+              url="https://nin-js.netlify.app/"
+              repoUrl="https://github.com/BrunoMalagoli/NinJS-Vite"
+            />
+            <ProjectCard
+              title="nin-js"
+              description="Web app estilo Quiz-game con preguntas de programación"
+              tech={[
+                <IconReact height={"3rem"} width={"3rem"} />,
+                <IconTypescript height={"3rem"} width={"3rem"} />,
+                <IconExpress height={"3rem"} width={"3rem"} />,
+                <IconMongodb height={"3rem"} width={"3rem"} />,
+              ]}
+              features={[""]}
+              url=""
+              repoUrl=""
+            />
+          </div>
+        </section>
+      </Fade>
     </Element>
   );
 };
